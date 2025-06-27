85adb7332f1d908d85d0a188419e95feba643efb
ahmed-elhdad <ahalhdad2023@gmail.com>
2025-06-27 09:53:34 +0300

    Create all files and folders for the preject

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className="navbar">
+    
       <Link to="/">الرئيسية</Link>
       <Link to="/kings">ملوك مصر</Link>
       <Link to="#">المعابد</Link>
